e80f188716b63b8a170551551555d5ea2854c8b2
Shobhan Akshay Giridharan <87001157+OPxKK@users.noreply.github.com>
2021-10-13 16:03:46 +0530

    Add files via upload

---
+++ College_db.sql
@@ -0,0 +1,22 @@
+/*
+-- Query: SELECT * FROM collegedb.users
+LIMIT 0, 1000
+
+-- Date: 2021-10-13 16:02
+*/
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('SamuelRobert','Sam123','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('DhanrajShetty','Sonu123','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('PayalThorat','Payal23','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('SnehaSable','Sneha21','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Nemil Sheth','Nemil123','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Nilesh Lad','Nilesh123','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Shilpa Chaman','Shilpa56','teacher');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Paul Fernandes','Paul78','teacher');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Kevin D\'Souza','KevinD','teacher');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Wilson Pinto','TpoOP','teacher');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Monica Cherra','Monica34','teacher');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Ramjee Yadav','Ramjee67','teacher');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Yuvraj Varnish','VarnishOP','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Viraj Thakur','LTI8.5OP','student');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Jayasudha Koti','Jayasudha9','teacher');
+INSERT INTO `` (`username`,`password`,`privilege`) VALUES ('Valentina Basker','Valentina45','teacher');
